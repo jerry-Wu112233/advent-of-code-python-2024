ae89568ed68892fb2368cad450806babed0a6365
jerru <yihanwu4@illinois.edu>
2024-12-09 18:58:57 -0500

    day 9 part 1

---
+++ day9/day9.py
@@ -0,0 +1,82 @@
+from itertools import groupby, islice, zip_longest
+
+with open("day9/input.txt", "r") as f:
+    disk_map = [int(n) for line in f for n in line.rstrip("\n")]
+
+class Block:
+    def __init__(self, val, start, length):
+        self.val = val
+        self.start = start
+        self.end = start + length - 1
+        self.length = length
+
+def part_1() -> int:
+    file_blocks = _disk_map_to_file_blocks()
+    for space_index, block_index in zip(
+        _disk_map_indices(find_space=True),
+        reversed(_disk_map_indices(find_space=False)),
+    ):
+        if block_index < space_index:
+            break
+        file_blocks[space_index] = file_blocks[block_index]
+        file_blocks[block_index] = "."
+
+    return sum(i * int(val) for i, val in enumerate(file_blocks) if val != ".")
+
+
+def part_2() -> int:
+    file_blocks = _disk_map_to_file_blocks()
+    condensed_repr_of_blocks = _group_with_indices(file_blocks)
+    space_info_arr = [t for t in condensed_repr_of_blocks if t.val == "."]
+    for block in reversed(condensed_repr_of_blocks):
+        if block.val == ".":
+            continue
+        
+        for space_block in space_info_arr:
+            if space_block.start > block.start:
+                break
+            if space_block.length < block.length:
+                continue
+            file_blocks[space_block.start: space_block.start + block.length - 1] = [block.val for _ in range(block.length)]
+            file_blocks[block.start: block.end + 1] = ["." for _ in range(block.length)]
+            space_block.length -= block.length
+            space_block.start += block.length - 1
+            break
+    print("".join(file_blocks))
+    return 0
+        
+
+def _group_with_indices(s: list[str]) -> list[Block]:
+    result = []
+    start = 0
+    for char, group in groupby(s):
+        length = len(list(group))
+        end = start + length - 1   
+        result.append(Block(char, start, length))
+        start += length
+    
+    return result
+
+def _disk_map_indices(find_space: bool) -> list[int]:
+    file_blocks = _disk_map_to_file_blocks()
+    if find_space:
+        return [i for i, val in enumerate(file_blocks) if val == "."]
+    return [i for i, val in enumerate(file_blocks) if val != "."]
+
+
+def _disk_map_to_file_blocks() -> list[str]:
+    blocks = [
+        (str(id_num), repetitions)
+        for id_num, repetitions in enumerate(islice(disk_map, 0, None, 2))
+    ]
+    spaces = [(".", repetitions) for repetitions in islice(disk_map, 1, None, 2)]
+    file_blocks = []
+    for pair in zip_longest(blocks, spaces, fillvalue=(".", 0)):
+        for symbol, repetitions in pair:
+            file_blocks.extend([symbol] * repetitions)
+    
+    return file_blocks
+
+
+print("00992111777.44.333....5555.6666.....8888..")
+print(part_2())
