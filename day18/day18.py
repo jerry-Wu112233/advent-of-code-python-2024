e8b6a17f8fcb6c9d4b620496c1399055389bf29f
jerru <yihanwu4@illinois.edu>
2024-12-18 01:10:21 -0500

    add day 18

---
+++ day18/day18.py
@@ -0,0 +1,47 @@
+import networkx as nx
+
+obstacles = set()
+width = 71
+height = 71
+coords_after_1024 = []
+with open("day18/input.txt", "r") as f:
+    for line_num, line in enumerate(f.read().strip().split("\n")):
+        x_coord, y_coord = map(int, line.rstrip("\n").split(","))
+        if line_num >= 1024:
+            coords_after_1024.append(complex(y_coord, x_coord))
+            continue
+        obstacles.add(complex(y_coord, x_coord))
+
+G = nx.Graph()
+
+directions = [-1, 1, -1j, 1j]
+for row in range(71):
+    for col in range(71):
+        pos = complex(row, col)
+        if pos in obstacles:
+            continue
+        for dir in directions:
+            neighbor = pos + dir
+            if not (0 <= neighbor.real < width and 0 <= neighbor.imag < height):
+                continue
+            if neighbor in obstacles:
+                continue
+            G.add_edge(pos, neighbor)
+
+def part_1() -> int:
+    return nx.shortest_path_length(G, source=complex(0,0), target=complex(70,70))
+
+def part_2() -> tuple[int, int]:
+    for obstacle in coords_after_1024:
+        for dir in directions:
+            neighbor = obstacle + dir
+            try:
+                G.remove_edge(obstacle, neighbor)
+            except nx.NetworkXError:
+                continue
+            obstacles.add(obstacle)
+        try:
+            nx.shortest_path_length(G, source=complex(0, 0), target=complex(70,70))
+        except nx.NetworkXNoPath:
+            return obstacle.imag, obstacle.real
+    return 0, 0
